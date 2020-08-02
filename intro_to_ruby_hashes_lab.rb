142eccf4b2806692075d3ae50f979f3e8397055a
Nick Bazaldua <bazalnick@gmail.com>
2020-08-02 20:51:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,11 +51,8 @@ monopoly = {
     :railroads => {
     :pieces => 4,
     :names => {
-      :reading_railroad => {"mortgage_value" => "$100"
-      },
-        "mortgage_value" => "$100"
-      },
-      :pennsylvania_railroad => {},
+      :reading_railroad => {"mortgage_value" => "$100"},
+      :pennsylvania_railroad => {"mortage_value" => "$200"},
       :b_and_o_railroad => {},
       :shortline => {}
     },
